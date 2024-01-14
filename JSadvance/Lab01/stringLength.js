ad61be6c601b57a6e21d76b02f2937305c277762
Angel <angel_tsvetkov_palamida@abv.bg>
2024-01-14 11:01:36 +0200

    Homework

---
+++ JSadvance/Lab01/stringLength.js
@@ -0,0 +1,6 @@
+function strLength(a,b,c) {
+    let alllength=a.length+b.length+c.length;
+    console.log(alllength);
+    console.log(Math.floor(alllength/3));
+}
+strLength('chocolate', 'ice cream', 'cake')
