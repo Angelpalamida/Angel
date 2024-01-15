31c88fe65a9ef971fc262ecce9360ea29416b9bf
Angel <angel_tsvetkov_palamida@abv.bg>
2024-01-15 17:34:10 +0200

    Update evenPositionElement.js

--- JSadvance/Lab/evenPositionElement.js
+++ JSadvance/Lab/evenPositionElement.js
@@ -1,6 +1,10 @@
 function evenPosition(arr) {
   let final = [];
-  let result = arr.map((a, i, b) =>i % 2 == 0? final.push(arr[i]) : undefined);
-  console.log(final.join(" "));
+  let sorting = arr.map((a, i, b) =>{
+    if(i%2===0){
+      final.push(arr[i]);
+    }
+  });
+  console.log(final.join(` `));
 }
 evenPosition(["20", "30", "40", "50", "60"]);
