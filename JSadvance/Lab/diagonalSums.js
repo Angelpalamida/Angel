1496c44e3d4325090a01c958ad04d92789bd8287
Angel <angel_tsvetkov_palamida@abv.bg>
2024-01-17 13:53:59 +0200

    Create diagonalSums.js

---
+++ JSadvance/Lab/diagonalSums.js
@@ -0,0 +1,16 @@
+function diagonalSums(matrix) {
+    let mainDiagonalSum = 0;
+    let secondaryDiagonalSum = 0;
+  
+    for (let i = 0; i < matrix.length; i++) {
+      mainDiagonalSum += matrix[i][i];
+      secondaryDiagonalSum += matrix[i][matrix.length - 1 - i];
+    }
+  
+    console.log(`${mainDiagonalSum} ${secondaryDiagonalSum}`);
+  }
+diagonalSums(
+    [[3, 5, 17],
+    [-1, 7, 14],
+    [1, -8, 89]]
+   );
