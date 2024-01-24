c283ad3a2a79a4da2840c6101684fc02218d56f3
Angel <angel_tsvetkov_palamida@abv.bg>
2024-01-24 19:44:07 +0200

    Update solution.js

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO...
+ let firstWord=document.getElementById("text").value;
+ let comand=document.getElementById("naming-convention").value;
+  let modifiedWord;
+  if(comand=="Camel Case"){
+
+   modifiedWord=firstWord
+   .split(' ')
+   .map((word, index) => index === 0 ? word.toLowerCase() : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+   .join('');
+  }else if(comand=="Pascal Case"){
+    modifiedWord = text.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('');
+
+  }else{
+    console.log(`Error!`);
+  }
+  document.getElementById('result').innerHTML = modifiedWord;
 }
