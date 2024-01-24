6566d62e75fc3ee18aca60748f4646cd15d08ce9
Angel <angel_tsvetkov_palamida@abv.bg>
2024-01-24 15:49:37 +0200

    Create showMore.js

---
+++ DOM/showMore.js
@@ -0,0 +1,16 @@
+function showText() {
+   
+    let link = document.getElementById('more');
+
+   
+    let text = document.getElementById('text');
+
+    
+   
+        
+        text.style.display = 'inline';
+
+      
+        link.style.display = 'none';
+    
+}
