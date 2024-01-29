306ec7478611d742147cd9cb355e91e1fa9f0550
Angel <angel_tsvetkov_palamida@abv.bg>
2024-01-29 11:40:41 +0200

    Update solution.js

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,18 +1,32 @@
-function solve() {
- let firstWord=document.getElementById("text").value;
- let comand=document.getElementById("naming-convention").value;
-  let modifiedWord;
-  if(comand=="Camel Case"){
+function solve(){
+    let textValue=document.getElementById(`text`).value;
+    let caller=document.getElementById(`naming-convention`).value;
+    let resultRef=document.getElementById(`result`);
 
-   modifiedWord=firstWord
-   .split(' ')
-   .map((word, index) => index === 0 ? word.toLowerCase() : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-   .join('');
-  }else if(comand=="Pascal Case"){
-    modifiedWord = text.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('');
+    textValue=textValue.toLowerCase();
+    result="";
+    switch(caller){
+      case`Camel Case`:
+      textArr=textValue.split(` `);
+      result=textArr.shift();
+      textArr.forEach(el => {
+        result+=el[0].toUpperCase()+el.substring(1).toLowerCase();
+      });
+      break;
+      case`Pascal Case`:
+      textArr=textValue.split(` `);
+      
+      textArr.forEach(el => {
+        result+=el[0].toUpperCase()+el.substring(1).toLowerCase();
+      });
+      break;
+      default :result=`Error!`
+    }
+    resultRef.textContent=result;
+    
 
-  }else{
-    console.log(`Error!`);
-  }
-  document.getElementById('result').innerHTML = modifiedWord;
 }
+
+
+
+
