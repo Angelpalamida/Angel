dd8e2d510397c12808d6e217175116582e25131a
Angel <angel_tsvetkov_palamida@abv.bg>
2024-01-29 15:25:26 +0200

    Update search.js

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,53 +1,40 @@
 function search() {
-  
   clearResults();
-
 
   const searchText = document.getElementById("searchText").value.toLowerCase();
 
- 
   const townsList = document.getElementById("towns").getElementsByTagName("li");
 
-  
   let matches = 0;
-  let resultHTML = '';
+  let resultHTML = "";
 
-  
   for (let i = 0; i < townsList.length; i++) {
-      const town = townsList[i];
-      const townText = town.textContent.toLowerCase();
+    const town = townsList[i];
+    const townText = town.textContent.toLowerCase();
 
-     
-      if (townText.includes(searchText)) {
-        
-          const strongElement = document.createElement("strong");
+    if (townText.includes(searchText)) {
+      const strongElement = document.createElement("strong");
 
-         
-          strongElement.appendChild(document.createTextNode(town.textContent));
+      strongElement.appendChild(document.createTextNode(town.textContent));
 
-          
-          town.innerHTML = '';
-          town.appendChild(strongElement);
+      town.innerHTML = "";
+      town.appendChild(strongElement);
 
-          matches++;
-      }
+      matches++;
+    }
   }
 
- 
-  resultHTML = matches + (matches === 1 ? ' match' : ' matches') + ' found';
+  resultHTML = matches + (matches === 1 ? " match" : " matches") + " found";
   document.getElementById("result").innerHTML = resultHTML;
 }
 
 function clearResults() {
-  
   const townsList = document.getElementById("towns").getElementsByTagName("li");
 
-  
   for (let i = 0; i < townsList.length; i++) {
-      const town = townsList[i];
-      town.innerHTML = town.textContent;
+    const town = townsList[i];
+    town.innerHTML = town.textContent;
   }
 
-  
-  document.getElementById("result").innerHTML = '';
+  document.getElementById("result").innerHTML = "";
 }
