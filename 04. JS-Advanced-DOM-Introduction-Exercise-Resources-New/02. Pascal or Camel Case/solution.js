b99d89864501dd6b076a5ef253c64acc5dcf99a9
Angel <angel_tsvetkov_palamida@abv.bg>
2024-01-29 12:37:20 +0200

    Update solution.js

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,32 +1,27 @@
-function solve(){
-    let textValue=document.getElementById(`text`).value;
-    let caller=document.getElementById(`naming-convention`).value;
-    let resultRef=document.getElementById(`result`);
+function solve() {
+  let textValue = document.getElementById(`text`).value;
+  let caller = document.getElementById(`naming-convention`).value;
+  let resultRef = document.getElementById(`result`);
 
-    textValue=textValue.toLowerCase();
-    result="";
-    switch(caller){
-      case`Camel Case`:
-      textArr=textValue.split(` `);
-      result=textArr.shift();
-      textArr.forEach(el => {
-        result+=el[0].toUpperCase()+el.substring(1).toLowerCase();
+  textValue = textValue.toLowerCase();
+  result = "";
+  switch (caller) {
+    case `Camel Case`:
+      textArr = textValue.split(` `);
+      result = textArr.shift();
+      textArr.forEach((el) => {
+        result += el[0].toUpperCase() + el.substring(1).toLowerCase();
       });
       break;
-      case`Pascal Case`:
-      textArr=textValue.split(` `);
-      
-      textArr.forEach(el => {
-        result+=el[0].toUpperCase()+el.substring(1).toLowerCase();
+    case `Pascal Case`:
+      textArr = textValue.split(` `);
+
+      textArr.forEach((el) => {
+        result += el[0].toUpperCase() + el.substring(1).toLowerCase();
       });
       break;
-      default :result=`Error!`
-    }
-    resultRef.textContent=result;
-    
-
+    default:
+      result = `Error!`;
+  }
+  resultRef.textContent = result;
 }
-
-
-
-
