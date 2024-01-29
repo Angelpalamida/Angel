180ce071b3cff685d71cedff95a8f4f2ca96e226
Angel <angel_tsvetkov_palamida@abv.bg>
2024-01-29 12:37:03 +0200

    Update accordion.js

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    console.log('TODO:...');
+    var extraContent = document.getElementById("extra");
+    var button = document.querySelector('.head .button');
+
+    if (extraContent.style.display === "block" || extraContent.style.display === "") {
+        // If the content is visible, hide it and change the button text to "More"
+        extraContent.style.display = "none";
+        button.innerHTML = "More";
+    } else {
+        // If the content is hidden, show it and change the button text to "Less"
+        extraContent.style.display = "block";
+        button.innerHTML = "Less";
+    }
 }
