a110d0bf0f7d22f34e2cbc251d3e8aec0bbceb04
Angel <angel_tsvetkov_palamida@abv.bg>
2024-01-29 13:39:52 +0200

    Update search.js

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,40 @@
 function search() {
-   // TODO
+  clearResults();
+
+  const searchText = document.getElementById("searchText").value.toLowerCase();
+
+  const townsList = document.getElementById("towns").getElementsByTagName("li");
+
+  let matches = 0;
+  let resultHTML = "";
+
+  for (let i = 0; i < townsList.length; i++) {
+    const town = townsList[i];
+    const townText = town.textContent.toLowerCase();
+
+    if (townText.includes(searchText)) {
+      const strongElement = document.createElement("strong");
+
+      strongElement.appendChild(document.createTextNode(town.textContent));
+
+      town.innerHTML = "";
+      town.appendChild(strongElement);
+
+      matches++;
+    }
+  }
+
+  resultHTML = matches + (matches === 1 ? " match" : " matches") + " found";
+  document.getElementById("result").innerHTML = resultHTML;
+}
+
+function clearResults() {
+  const townsList = document.getElementById("towns").getElementsByTagName("li");
+
+  for (let i = 0; i < townsList.length; i++) {
+    const town = townsList[i];
+    town.innerHTML = town.textContent;
+  }
+
+  document.getElementById("result").innerHTML = "";
 }
