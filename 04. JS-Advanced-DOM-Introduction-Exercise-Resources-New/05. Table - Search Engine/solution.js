b36ddd801bdc39eaa7c9c75daa78b8a4fe1318f1
Angel <angel_tsvetkov_palamida@abv.bg>
2024-01-29 18:21:10 +0200

    Update solution.js

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -1,8 +1,21 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
-
+   const surchEl=document.getElementById(`searchField`);
+   const tableRolls=document.querySelectorAll(`tbody tr`);
+   
    function onClick() {
-      //   TODO:
+     let surch=surchEl.value;
+     for (const row of tableRolls) {
+         let tableData=row.querySelectorAll("td");
+         for (const data of tableData) {
+            if(data.textContent.includes(surch)){;
+            row.classList.add("select");
+            break;
+         }else{
+            row.classList.remove("select")
+         }
+      }
+     }
 
    }
 }
