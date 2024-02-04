e3894f467e58c230733c34b65feeb3d9d09ca576
Angel <angel_tsvetkov_palamida@abv.bg>
2024-02-04 09:02:37 +0200

    Update solution.js

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-  //TODO
+  
+  const inputText = document.getElementById("input").value;
+
+ 
+  const sentences = inputText.split('.').filter(sentence => sentence.trim().length > 0);
+
+  
+  const outputDiv = document.getElementById("output");
+  
+
+
+  for (let i = 0; i < sentences.length; i += 3) {
+      const paragraphText = sentences.slice(i, i + 3).join('. ');
+
+     
+      const paragraph = document.createElement("p");
+      
+      paragraph.textContent = paragraphText;
+
+      
+      outputDiv.appendChild(paragraph);
+  }
+ 
 }
