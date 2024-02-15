9e3f26cabb8234eb15b4df2dd82dd7735ccd4957
Angel <angel_tsvetkov_palamida@abv.bg>
2024-02-15 17:59:09 +0200

    Update 03_test.js

---
+++ JSadvance/examPrep/03_test.js
@@ -0,0 +1,9 @@
+import{ expect } from `chai`;
+import{motorcycleRider} from `./MotorcycleRider.js`;
+
+
+describe(`Test suite`,function () {
+    it(`shuld load libraries`,()=>{
+        expect(1).to.equal(1);
+    })
+})
