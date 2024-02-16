674801431a22ad5665057df6ee11948d07e20762
Angel <angel_tsvetkov_palamida@abv.bg>
2024-02-16 15:23:29 +0200

    Delete 03.test.js

--- JSadvance/examPrep/03.test.js
+++
@@ -1,8 +0,0 @@
-import {expect}from`chai`;
-import{motorcycleRider}from`./Motorcycle rider.js`;
-
-discribe(`Test suite`,function(){
-    it(`Shuld load libreries`,()=>{
-        expect(1).to.equal(1);
-    });
-});
