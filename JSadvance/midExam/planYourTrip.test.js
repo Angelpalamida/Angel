9083c91c0ead9da8e89b54abfa77850c2d8c7bd1
Angel <angel_tsvetkov_palamida@abv.bg>
2024-02-17 17:00:09 +0200

    Update planYourTrip.test.js

--- JSadvance/midExam/planYourTrip.test.js
+++ JSadvance/midExam/planYourTrip.test.js
@@ -38,9 +38,26 @@ describe("Tests for planYourTrip", function () {
             expect(planYourTrip.estimateExpenses(200, 4)).to.equal("The estimated cost for the trip is $800.00, plan accordingly.");
         });
 
-        it("should throw an error for not numbers, or are negative numbers or zero", function () {
+        it("should throw an error for not numbers", function () {
             expect(() => planYourTrip.estimateExpenses("100", 4)).to.throw("Invalid Information!");
+            
+        });
+        it("should throw an error for negative numbers", function () {
+            
             expect(() => planYourTrip.estimateExpenses(100, -4)).to.throw("Invalid Information!");
+           
+        });
+        it("should throw an error for zero", function () {
+            
+            expect(() => planYourTrip.estimateExpenses(0, 0)).to.throw("Invalid Information!");
+        });
+        it("should throw an error for array", function () {
+            
+            expect(() => planYourTrip.estimateExpenses([1], 5)).to.throw("Invalid Information!");
+        });
+        it("should throw an error for array2", function () {
+            
+            expect(() => planYourTrip.estimateExpenses(1, [5])).to.throw("Invalid Information!");
         });
     });
 });
