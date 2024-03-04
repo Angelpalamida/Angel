70e1fef199301cce24ff79f15668a5deb8cf3221
Angel <angel_tsvetkov_palamida@abv.bg>
2024-03-04 08:59:38 +0200

    Update app.js

--- aplications/asinchroniusPrograming/03.Gighub-Commits/app.js
+++ aplications/asinchroniusPrograming/03.Gighub-Commits/app.js
@@ -1,4 +1,34 @@
+const list=document.getElementById('commits')
+
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const username=document.getElementById(`username`).value;
+    const repo=document.getElementById('repo').value;
+   
+    const url=`https://api.github.com/repos/${username}/${repo}/commits`;
+
+    fetch(url)
+    .then(OnSuccess)
+    .then(addLi)
+    .catch(OnError)
 }
+
+    function OnSuccess(response) {
+        
+        if(!response.ok){
+            throw 'Error'
+        }
+        return response.json();
+    }
+
+    function OnError(error){
+        list.innerHTML='<li>Error: 404 (Not Found)</li>'
+        
+    }
+    function addLi(data){
+        list.replaceChildren(...data.map(createListItems));
+    }
+    function createListItems({commit:{author: {name}, message}}){
+        const item=document.createElement('li');
+        item.textContent=`${name}:${message}`;
+        return item;
+    }
